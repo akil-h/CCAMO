108e7814ef7c4f4bce3f89aad7550261b3fd879e
Akil Huang <akil.huang@mail.utoronto.ca>
2024-04-24 00:14:33 -0400

    Made changes to file structure

--- Notebooks/prediction.py
+++ Notebooks/prediction.py
@@ -1,1 +1,14 @@
-import numpy as np
+import os
+import h5py
+
+# 1. Create h5py file
+with h5py.File('data.h5', 'w') as hdf_file:
+    text_dataset = hdf_file.create_dataset('articles', shape=(0,), maxshape=(None,), dtype=h5py.special_dtype(vlen=str))
+    new_org_dataset = hdf_file.create_dataset('news_orgs', shape=(0,), maxshape=(None,), dtype=h5py.special_dtype(vlen=str))
+
+    # Iterate over files
+
+# 2. Split data
+
+# 3. Train CNN/SVM/GBM on input text & political orientation labels
+
